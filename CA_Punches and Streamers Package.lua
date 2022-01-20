e9c4ad9b5a947a4a0ff7e3e971e7601edbdf4037
Christian Afonso <christian.afonso@wohlhart.at>
2022-01-20 23:15:55 +0100

    WIP Updated reapack_index.xml, references new metapackage script - try if everything lands in correct dirs

---
+++ CA_Punches and Streamers Package.lua
@@ -0,0 +1,41 @@
+--[[
+  @description CA_Punches_and_Streamers
+  @author ChrAfonso
+  @version 0.4
+  @about
+    This script collection provides functionality to overlay punches and colored
+	streamers over the project video for film scoring synchronization and ADR.
+	Two workflows are currently supported: Generate punches and streamers from
+	custom markers, or use the included toolbar to place them manually.
+	
+	More information in the included files README.md and
+	doc/CA_Punches_and_Streamers_README.txt
+	
+  @metapackage
+  @provides
+    [main] CA_add custom streamer.lua'
+    [main] CA_add flutter.lua'
+    [main] CA_add green streamer 2s.lua'
+    [main] CA_add green streamer 3s.lua'
+    [main] CA_add green streamer custom length.lua'
+    [main] CA_add punch.lua'
+    [main] CA_add red streamer 2s.lua'
+    [main] CA_add red streamer 3s.lua'
+    [main] CA_add white streamer 2s.lua'
+    [main] CA_add white streamer 3s.lua'
+    [main] CA_add yellow streamer 2s.lua'
+    [main] CA_add yellow streamer 3s.lua'
+    [main] CA_show streamers list.lua'
+    [main] CA_streamer settings disable end inside.lua'
+    [main] CA_streamer settings disable start inside.lua'
+    [main] CA_streamer settings enable end inside.lua'
+    [main] CA_streamer settings enable start inside.lua'
+    [main] CA_streamer settings toggle end inside.lua'
+    [main] CA_streamer settings toggle start inside.lua'
+    [main] CA_update click track.lua'
+    CA_streamers_lib.lua
+	data/*
+	[data] toolbar/icons/*
+	FilmSync.ReaperMenu > MenuSets/
+
+]]--
